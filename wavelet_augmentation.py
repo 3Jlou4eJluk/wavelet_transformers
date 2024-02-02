fd690db49cfcac898f699089347bc14196731706
Mikhail <3lou4yballl@gmail.com>
2024-02-02 21:42:51 +0300

    Wavelet Augmentator upgrade. Now it acts randomely.

--- wavelet_augmentation.py
+++ wavelet_augmentation.py
@@ -6,14 +6,16 @@ import ptwt
 from functools import partial
 from einops import rearrange
 from einops import reduce
-from einops import repeat
 
 class WaveletAugmentation:
-    def __init__(self, levels, trade_off=4, wavelet='db1', verbose=False):
+    def __init__(self, levels, proba=0.5, complexity_range=(1, 16), wavelet='db1', verbose=False):
         self.wavelet = wavelet
-        self.trade_off = trade_off
+        self.complexity_range = complexity_range
         self.levels = levels
         self.verbose = verbose
+        self.proba = proba
+
+        assert (proba >= 0.) and (proba <= 1.), 'Probability must be between 0. and 1.'    
         self.wt_transform = partial(
             ptwt.wavedec2, wavelet=pywt.Wavelet(wavelet), 
             level=levels, mode='constant'
@@ -21,17 +23,22 @@ class WaveletAugmentation:
         self.inverse_wt_transform = partial(
             ptwt.waverec2, wavelet=pywt.Wavelet(wavelet)
         )
-    def __call__(self, img):
-        coefs = self.wt_transform(img)
 
+    def __call__(self, img):
+        if not np.random.binomial(1, self.proba, 1)[0]:
+            return img
+        coefs = self.wt_transform(img)
         for i in range(self.levels):
             part = coefs[-1 - i]
             tens = rearrange(list(part), 'n c h w -> (n c) h w')
             abs = torch.abs(tens)
-            thresholds = reduce(abs / self.trade_off, 'c h w -> c 1 1', 'max')
-            mask = (abs <= thresholds)
+            threshold = 2 ** (torch.log2(reduce(abs, 'c h w -> c 1 1', 'max'))).to(torch.float)
+            trade_off = torch.rand(1) * (self.complexity_range[1] - self.complexity_range[0]) \
+                        + self.complexity_range[0]
+            threshold /= trade_off
+            mask = (abs <= threshold)
             if self.verbose:
-                print(f'Note: {mask.sum()} elements are zero now')
+                print(f'Note: {mask.sum()} elements are zero now.')
             tens[mask] = 0
             back = rearrange(tens, '(n c) h w -> n c h w', c=3)
             coefs[-1 - i] = tuple([back[j] for j in range(3)])
