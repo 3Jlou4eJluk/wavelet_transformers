403b3b41d2fc5e8deabed330efcfd1fe7d374bfc
Mikhail <3lou4yballl@gmail.com>
2024-02-10 23:55:16 +0300

    kSVD Tokenizer Update

--- preprocessing_tools.py
+++ preprocessing_tools.py
@@ -125,94 +125,85 @@ def build_batch_features(
     return tok_repr, labels, total_token_count
 
 
-# @title kSVD
-from torch.nn.functional import pad
+
+from einops import rearrange, repeat
 from sklearn.decomposition import MiniBatchDictionaryLearning
-from einops import rearrange
 
-
-class kSVD_Vectorizer:
+class kSVD_Tokenizer(torch.nn.Module):
     def __init__(
-            self, batch_count=100, patch_size=2,
-            n_components=128, batch_size=4, transform_algorithm='lasso_lars',
-            transform_alpha=0.1, max_iter=5, random_state=42
+            self, n_components, patch_size, img_size, dlearner_batch_size=4,
+            emb_dim=256, verbose=False, device='cpu'
         ):
-        self.patch_size = patch_size
-        self.batch_count = batch_count
-        self.n_components = n_components
-        self.batch_size = batch_size
-        self.transform_algorithm = transform_algorithm
-        self.transform_alpha = transform_alpha
-        self.max_iter = max_iter
-        self.random_state = random_state
+        super().__init__()
+        '''
+            Expected input: images in format YCbCr with shape = (bs, c, h, w)
+        '''
 
-        self.emb_dim = n_components
+        self.args = {
+            'n_components': n_components,
+            'patch_size': patch_size,
+            'img_size': img_size,
+            'dlearner_batch_size': dlearner_batch_size,
+            'verbose': verbose,
+            'device': device
+        }
 
-        # Инициализируем массивы для хранения патчей для каждого канала
-        self.channel_patches = [np.array([]).reshape(0, self.patch_size, self.patch_size) for _ in range(3)]  # [R_patches, G_patches, B_patches]
-
-        # Патчи теперь лежат в channel_patches[0], channel_patches[1], and channel_patches[2]
-        self.dlearners = [
-            MiniBatchDictionaryLearning(
-                n_components=n_components, batch_size=batch_size, transform_algorithm=transform_algorithm,
-                transform_alpha=transform_alpha, max_iter=max_iter, random_state=random_state, tol=1e-3
-            ) for i in range(3)
-        ]
-
-        self.coders = None
-
-
-    def fit(self, trainloader):
-        print(f'Note: batch_count is {self.batch_count}')
-        print('Extracting patches')
-
-        self.channel_patches = [[], [], []]
-        for batch_idx, (images, _) in enumerate(tqdm(trainloader)):
-            # Преобразуем одно изображение в патчи размером 2x2
-            for im_no in range(len(images)):
-                patches = self.image_to_patches(images[im_no])  # размер (num_patches, C, patch_size, patch_size)
-                for c in range(3):  # Перебираем каждый канал цвета
-                    self.channel_patches[c].extend(patches[:, c, :, :].numpy())
-                    #self.channel_patches[c] = np.vstack((self.channel_patches[c], patches[:, c, :, :].numpy()))
-
-            # Для теста возьмём только первые несколько изображений
-            if batch_idx == self.batch_count:  # после 10 изображений остановимся
-                print(f"Batch count {self.batch_count} reached. Stopping fit process.")
-                break
-        self.channel_patches = np.array(self.channel_patches)
-
-
-        print(f'shape is {self.channel_patches.shape}')
-        # Давай посмотрим, что у нас получилось
-        for i, channel in enumerate(["R", "G", "B"]):
-            print(f"Channel {channel}, shape: {self.channel_patches[i].shape}")
-
-        print('Learning dictionary')
-        for i in range(3):
-            self.dlearners[i].fit(self.channel_patches[i].reshape((self.channel_patches[i].shape[0], -1)))
-
-    def transform(self, images, verbose=False):
-        # extracting patches
-        bs = images.shape[0]
-        patches = rearrange(
-            images, "b c (h hp) (w wp) -> c (b h w) (hp wp)", 
-            hp=self.patch_size, wp=self.patch_size
+        self.dict_learner = MiniBatchDictionaryLearning(
+            n_components, transform_algorithm='omp',
+            max_iter=4, transform_n_nonzero_coefs= None,
+            transform_alpha=None, batch_size=dlearner_batch_size
         )
 
-        rf = self.dlearners[0].transform(patches[0, :, :])
-        gf = self.dlearners[1].transform(patches[1, :, :])
-        bf = self.dlearners[2].transform(patches[2, :, :])
-        res = rearrange([rf, gf, bf], "c (b pc) vl -> b c pc vl", b=bs)
-        return res
+        self.linear_projection = torch.nn.Linear(
+            n_components, emb_dim
+        )
 
-    def image_to_patches(self, image):
-        # image - тензор PyTorch (C, H, W)
-        C, H, W = image.size()
+        self.c_projection = torch.nn.Linear(
+            2 * (patch_size ** 2), emb_dim
+        )
 
-        # Теперь разделим на патчи
-        patches = image.unfold(1, self.patch_size, self.patch_size).unfold(2, self.patch_size, self.patch_size)
-        patches = patches.contiguous().view(C, -1, self.patch_size, self.patch_size)
+    def fit(self, imgs):
+        '''
+            Expected imgs.shape = (im_no, c, h, w) in YCbCr format.
+        '''
+        patches = rearrange(
+            imgs[:, 0, :, :], 'im_no (h ph) (w pw) -> (im_no h w) (ph pw)',
+            ph=self.args['patch_size'], pw=self.args['patch_size']
+        )
 
-        # Переставим размерности
-        return patches.permute(1, 0, 2, 3)
+        if self.args['verbose']:
+            print('Started dictionary learning... ', end='')
 
+        self.dict_learner.fit(patches)
+
+        if self.args['verbose']:
+            print("Done")
+
+
+    def forward(self, imgs):
+        # Decompose Y component and add color embeddings
+        ps = self.args['patch_size']
+        bs = imgs.shape[0]
+        patches = rearrange(
+            imgs[:, 0, :, :], "b (h ph) (w pw) -> (b h w) (ph pw)", ph=ps, pw=ps
+        )
+        transformed = self.dict_learner.transform(patches)
+
+        main_patches = rearrange(
+            transformed, "(b pc) pe -> b pc pe", b=bs
+        )
+
+        color_patches = rearrange(
+            imgs[:, 1:, :, :], 'b c (h ph) (w pw) -> b (h w) (c ph pw)', ph=ps, pw=ps
+        )
+
+        main_patches, color_patches = torch.tensor(main_patches).float(), torch.tensor(color_patches).float()
+
+        if self.args['device'] is not 'cpu':
+            main_patches = main_patches.to(self.args['device'])
+            color_patches = color_patches.to(self.args['device'])
+
+        trans_mp = self.linear_projection(main_patches)
+        trans_cp = self.c_projection(color_patches)
+
+        return trans_mp + trans_cp
