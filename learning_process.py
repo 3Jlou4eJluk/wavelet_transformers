5db14ba597f0bb15c4df907976ac75be2e0bcdb7
Mikhail <3lou4yballl@gmail.com>
2024-02-29 18:55:20 +0300

    learning process update

--- learning_process.py
+++ learning_process.py
@@ -5,6 +5,7 @@ import pickle
 import time
 from functools import partial
 from torch import nn
+import pandas as pd
 
 
 def xavier_init(m):
@@ -15,45 +16,41 @@ def xavier_init(m):
     elif isinstance(m, nn.LayerNorm):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
-
+            
 
 class ModelCheckpoint:
-    def __init__(self, filepath=None, disable=False):
+    def __init__(self, filepath=None):
         self.val_acc = None
         self.val_loss = None
         self.train_acc = None
         self.train_loss = None
         self.filepath = filepath
-        self.disable = disable
         if filepath is None:
             print('<Model Checkpoint>: Warning, model save path is not specified. Setting current directory.')
             self.filepath = '.'
-    
+
     def update(self, model, train_acc, train_loss, val_acc, val_loss):
-        if self.disable:
-            return None
         if (self.val_acc is None) or (val_acc > self.val_acc):
             self.val_acc = val_acc
             self.val_loss = val_loss
             self.train_acc = train_acc
             self.train_loss = train_loss
             self.save(model)
-    
+
 
     def save(self, model):
         torch.save(model, f"{self.filepath}/model_save.pt")
         with open(f'{self.filepath}/model_checkpoint.pkl', 'wb') as f:
             pickle.dump(self, f)
-    
-    def load(self, load_model=False):
+
+    def load(self):
         with open(f'{self.filepath}/model_checkpoint.pkl', 'rb') as f:
             loaded_object = pickle.load(f)
             self.val_acc = loaded_object.val_acc
             self.val_loss = loaded_object.val_loss
             self.train_acc = loaded_object.train_acc
             self.train_loss = loaded_object.train_loss
-        if load_model:
-            return torch.load(f"{self.filepath}/model_save.pt")
+        return torch.load(f"{self.filepath}/model_save.pt")
 
     def __repr__(self):
         return f"<Model Checkpoint>\n"\
@@ -61,152 +58,111 @@ class ModelCheckpoint:
              + f"Validation Loss: {self.val_loss}\n"\
              + f"Train Accuracy: {self.train_acc}\n"\
              + f"Train Loss: {self.train_loss}\n"
-        
 
 
-class Preprocessor:
-    def __init__(self, objects, labels, *funcs):
-        self.objects = objects
-        self.labels = labels
-        self.funcs = [func for func in funcs]
+class Actor:
+    def __init__(self, thres=420, sleep_time=120):
+        self.prev_time = None
+        self.thres = thres
+        self.sleep_time = sleep_time
 
-        self.preprocessed_objects = None
-    
-    def action(self):
-        self.preprocessed_objects = self.objects
+    def action(self, ):
+        if self.prev_time is None:
+            self.prev_time = time.time()
 
-        # Preprocessing data
-        for f in self.funcs:
-            self.preprocessed_objects = f(self.preprocessed_objects)
-        return self.preprocessed_objects
+        if time.time() - self.prev_time > self.thres:
+            self.thres = 0
+            time.sleep(self.sleep_time)
 
+class Logger:
+    def __init__(self, epochs, steps_per_epoch, checkpoints_module=None):
+        self.data_df = pd.DataFrame(
+            None, columns=['comment', 'step_no', 'loss', 'acc']
+        )
+        self.last_global_step = None
+        self.checkpoints = checkpoints_module
 
-class FrozenValueDetector:
-    def __init__(self, epochs_without_change):
-        self.check_epochs = epochs_without_change
-        self.prev_value = None
-        self.equal_epochs = 0
-    
-    def step(self, value):
-        if value == self.prev_value:
-            self.equal_epochs += 1
-        else:
-            self.equal_epochs = 0
-            self.prev_value = value
-
-        if self.equal_epochs == self.check_epochs:
-            raise ValueError
-
-
-class EarlyStopping:
-    """Ранняя остановка для остановки обучения, когда ошибка валидации перестает уменьшаться."""
-    def __init__(self, patience=7, verbose=False, delta=0):
-        """
-        Args:
-            patience (int): Количество эпох без улучшения после которых обучение будет прекращено.
-            verbose (bool): Включает вывод сообщений о ранней остановке.
-            delta (float): Минимальное изменение между эпохами для рассмотрения как улучшение.
-        """
-        self.patience = patience
-        self.verbose = verbose
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
-
-    def __call__(self, val_loss, model):
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-        elif score < self.best_score + self.delta:
-            self.counter += 1
-            print(f'EarlyStopping counter: {self.counter} из {self.patience}')
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_score = score
-            self.counter = 0
-
-        if self.early_stop and self.verbose:
-            print("Ранняя остановка выполнена")
+    def log_data(self, data):
+        print(f'Logger: {data}')
+        for key in data.keys():
+            data[key] = [data[key], ]
+        self.data_df = pd.concat((self.data_df, pd.DataFrame.from_dict(data)), ignore_index=True)
 
 
 class Learner:
     def __init__(
             self, model, optimizer, loss_fn, scheduler,
-            train_dl, val_dl, device, epochs, checkpoint_path=None,
-            max_training_time=None, chill_time=120, early_stop=None,
-            disable_checkpoints=False
-        ):
-        self.metrics = {
-            "train_loss": [],
-            "val_loss": [],
-            "train_acc": [],
-            "val_acc": [],
-            "train_loss_epoch_no": [],
-            "val_loss_epoch_no": []
-        }
-
+            train_dataloader, val_dataloader, device, epochs,
+            actor=None, logger=None, log_period=0.5
+    ):
         self.model = model
         self.optimizer = optimizer
         self.loss_fn = loss_fn
         self.scheduler = scheduler
-        self.checkpoint_path = checkpoint_path
-        self.max_training_time = max_training_time
-        self.chill_time = chill_time
-        self.early_stop = early_stop
 
-        self.train_dl = train_dl
-        self.val_dl = val_dl
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
 
+        self.log_period = log_period
         self.epochs = epochs
         self.device = device
 
 
         # service fields
-        self.model_checkpoint = ModelCheckpoint(self.checkpoint_path, disable=disable_checkpoints)
-        self.current_work_time = 0
+        self.actor = actor
+        self.logger = logger
 
-    def train_epoch(self, log_train_quality=False, verbose=False, epoch_no=None):
+        self.prev_log_step = 0
+
+
+    def train_epoch(self, log_train_quality=False, verbose=False, epoch_no=0):
         self.model.train()
 
         loss_sum = []
         acc_sum = []
-        for x, y in tqdm(self.train_dl, disable=not verbose, leave=True):
-            start_time = time.time()
+        for batch_no, (x, y) in enumerate(self.train_dataloader):
             if 'cpu' not in self.device:
-                x = x.to(self.device)
+                x = x
                 y = y.to(self.device)
-            def closure(x, y, loss_sum, acc_sum):
-                self.optimizer.zero_grad()
-                pred = self.model.forward(x)
-                loss = self.loss_fn(pred, y.squeeze())
-                loss.backward()
 
-                pred_classes = torch.argmax(pred, dim=1)
-                loss_sum.append(float(loss.item()))
-                acc_sum.append(float((
-                    pred_classes.detach().squeeze() == y.squeeze()
-                ).sum() / len(y)))
 
-            closure = partial(closure, x, y, loss_sum, acc_sum)
-            self.optimizer.step(closure)
+            self.optimizer.zero_grad()
+            pred = self.model.forward(x)
+            loss = self.loss_fn(pred, y.squeeze())
+            loss.backward()
+
+            pred_classes = torch.argmax(pred, dim=1)
+            loss_sum.append(float(loss.item()))
+            acc_sum.append(float((
+                pred_classes.detach().squeeze() == y.squeeze()
+            ).sum() / len(y)))
+
+            global_step = epoch_no * len(self.train_dataloader) + batch_no
+            if (self.logger is not None) \
+                and (global_step >= int(len(self.train_dataloader) * self.log_period) + self.prev_log_step):
+
+                self.prev_log_step = global_step
+                self.logger.log_data(
+                    {
+                        'comment'       : 'training_process',
+                        'step_no'       : global_step,
+                        'loss'          : sum(loss_sum) / len(loss_sum),
+                        'acc'           : sum(acc_sum) / len(acc_sum)
+                    }
+                )
+
+            self.optimizer.first_step(zero_grad=True)
+            self.loss_fn(self.model.forward(x), y.squeeze())
+            self.optimizer.second_step(zero_grad=True)
+
             if self.scheduler is not None:
                 self.scheduler.step()
-            self.current_work_time += time.time() - start_time
-            if (self.max_training_time is not None) and (current_work_time >= self.max_training_time):
-                current_work_time = 0
-                time.sleep(self.chill_time)
+
+            if self.actor is not None:
+                self.actor.action()
 
         loss_sum_val = sum(loss_sum) / len(self.train_dl)
         acc_sum_val = sum(acc_sum) / len(self.train_dl)
-
-        if log_train_quality and (epoch_no is not None):
-            self.metrics['train_loss_epoch_no'].append(epoch_no)
-            self.metrics['train_loss'].append(loss_sum_val)
-            self.metrics['train_acc'].append(acc_sum_val)
 
 
     def validation_epoch(self, log_train_quality=False, verbose=False, epoch_no=None):
@@ -218,9 +174,9 @@ class Learner:
         val_acc = 0.
         with torch.no_grad():
             if log_train_quality:
-                for x, y in tqdm(self.train_dl, disable=not verbose, leave=True):
+                for x, y in self.train_dataloader:
                     if 'cpu' not in self.device:
-                        x = x.to(self.device)
+                        x = x
                         y = y.to(self.device)
                     pred = self.model.forward(x)
                     pred_classes = torch.argmax(pred, dim=1)
@@ -233,9 +189,9 @@ class Learner:
                 train_loss /= len(self.train_dl)
                 train_acc /= len(self.train_dl)
 
-            for x, y in tqdm(self.val_dl, disable=not verbose):
+            for x, y in tqdm(self.val_dataloader, disable=not verbose):
                 if 'cpu' not in self.device:
-                    x = x.to(self.device)
+                    x = x
                     y = y.to(self.device)
                 pred = self.model.forward(x)
                 pred_classes = torch.argmax(pred, dim=1)
@@ -249,77 +205,38 @@ class Learner:
             val_acc /= len(self.val_dl)
             val_loss /= len(self.val_dl)
 
-            self.early_stop(val_loss, self.model)
-            if self.early_stop.early_stop:
-                # прекращаем обучение
-                return None
+        if self.logger is None:
+            return None
 
         if epoch_no is not None:
             if log_train_quality:
-                self.metrics['train_loss_epoch_no'].append(epoch_no)
-                self.metrics['train_loss'].append(train_loss)
-                self.metrics['train_acc'].append(train_acc)
+                self.logger.log_data(
+                    {
+                        'comment' : 'validation_train',
+                        'step_no' : epoch_no,
+                        'loss'    : train_loss,
+                        'acc'     : train_acc
+                    }
+                )
 
-            self.metrics['val_loss_epoch_no'].append(epoch_no)
-            self.metrics['val_loss'].append(val_loss)
-            self.metrics['val_acc'].append(val_acc)
-            self.model_checkpoint.update(
-                self.model, train_acc, train_loss,
-                val_acc, val_loss
+            self.logger.log_data(
+                {
+                    'comment' : 'validation_test',
+                    'step_no' : epoch_no,
+                    'loss'    : val_loss,
+                    'acc'     : val_acc
+                }
             )
+
 
     def train_cycle(self):
         for epoch in (pbar := tqdm(range(self.epochs), total=self.epochs, disable=False)):
             self.train_epoch(log_train_quality=True, verbose=True, epoch_no=epoch)
             if not epoch % 1:
                 self.validation_epoch(log_train_quality=False, verbose=True, epoch_no=epoch)
-                pbar.set_description(('Loss (Train/Test): {0:.3f}/{1:.3f}.\n' +\
-                                     'Accuracy,% (Train/Test): {2:.2f}/{3:.2f}.\n' +\
-                                     'Update Epoch: {4}').format(
-                    self.metrics['train_loss'][-1], self.metrics['val_loss'][-1],
-                    self.metrics['train_acc'][-1], self.metrics['val_acc'][-1],
-                    epoch
-                ))
+
 
     def train(self):
         if 'cpu' not in self.device:
             self.model.to(self.device)
         self.train_cycle()
-
-class BruteForcer:
-    def __init__(self):
-        pass
-
-
-class ExperimentResult:
-    def __init__(
-            self, train_milestones, 
-            val_milestones, train_acc, 
-            val_acc, train_loss, val_loss,
-            save_path='./', prefix=''
-    ):
-        self.train_milestones = train_milestones
-        self.val_milestones = val_milestones
-        self.train_acc = train_acc
-        self.val_acc = val_acc
-        self.train_loss = train_loss
-        self.val_loss = val_loss
-    
-        self.save_path = save_path
-        self.prefix = prefix
-
-    def save(self):
-        with open(self.save_path + self.prefix + 'exp_res.pickle', 'wb') as f:
-            pickle.dump(self, f)
-
-    def load(self):
-        with open(self.save_path + self.prefix + 'exp_res.pickle', 'rb') as f:
-            obj = pickle.load(f)
-
-        self.train_milestones = obj.train_milestones
-        self.val_milestones = obj.val_milestones
-        self.train_acc = obj.train_acc
-        self.val_acc = obj.val_acc
-        self.train_loss = obj.train_loss
-        self.val_loss = obj.val_loss
-
